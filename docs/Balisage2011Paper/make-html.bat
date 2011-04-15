18a865a5073a9d5cdec1117cf30ca5ee94a7062f
Chris Maloney <voldrani@gmail.com>
2011-04-15 03:28:08 +0000

    Updates to Balisage paper infrastructure

---
+++ docs/Balisage2011Paper/make-html.bat
@@ -0,0 +1,4 @@
+xsltproc --novalid ^
+  --stringparam css-file balisage-1-2-xsl/balisage-proceedings.css ^
+  balisage-1-2-xsl/balisage-proceedings-html.xsl Bal2011malo0713.xml ^
+  > Bal2011malo0713.html
